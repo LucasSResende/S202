f653b51df4adee7832dab525e5f049d9c4011826
Lucas de Souza Resende <99922083+LucasSResende@users.noreply.github.com>
2022-09-04 19:27:48 -0300

    Atividade 06
    Referente ao relatório 05

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from unicodedata import name
+from db import database
 from db.database import Database
 from helper.WriteAJson import writeAJson
 
-print("aula Mongo db")
+db = Database(database="database", collection="livros", dataset=database)
+
+
+lvrs = db.create(1, "Como fazer amigos e influenciar pessoas ", "Dale Carnegie", 1936, 39.9)
+lvrs = db.create(2, "A revolta de Atlas", "Ayn Rand", 1957, 71.91)
+lvrs = db.create(3, "O caminho da servidão", "F.A Hayek", 1944, 46.95)
+
+# Atualizando os dados do livro 2
+lvrs = db.update(2, (56.90))
+# Vou deletar o livro 3 só para teste
+
+lvrs = db.delete(3)
+
+lvrs = db.read()
+writeAJson(lvrs, "Todos os livros")
